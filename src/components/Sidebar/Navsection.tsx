0f2b87c95a5aff1d6359fd3a5e690a68de8d9acf
Felipe Moura <felipemimoura@gmail.com>
2022-03-04 08:49:23 -0300

    refactor: melhora a componentiação da sidebar

---
+++ src/components/Sidebar/Navsection.tsx
@@ -0,0 +1,20 @@
+import { Box, Link, Stack, Text } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface NavsectionProps {
+  title: string;
+  children: ReactNode;
+}
+
+export default function Navsection({ title, children }: NavsectionProps) {
+  return (
+    <Box>
+      <Text fontWeight="bold" color="gray.400" fontSize="small">
+        {title}
+      </Text>
+      <Stack spacing="4" mt="8" align="stretch">
+        {children}
+      </Stack>
+    </Box>
+  );
+}
