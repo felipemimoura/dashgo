052d93777ce591bad113549c28504f0d5a2142b6
Felipe Moura <felipemimoura@gmail.com>
2022-03-07 07:36:09 -0300

    refactor: adicona componente active link ao invés de utilizar o Link do next

--- src/components/Sidebar/Navlink.tsx
+++ src/components/Sidebar/Navlink.tsx
@@ -1,11 +1,11 @@
 import {
   Icon,
   Link as ChakraLink,
-  Text,
   LinkProps as ChakraLinkProps,
+  Text,
 } from "@chakra-ui/react";
-import Link from "next/link";
 import { ElementType } from "react";
+import ActiveLink from "../ActiveLink";
 
 interface NavlinkProps extends ChakraLinkProps {
   icon: ElementType;
@@ -20,13 +20,13 @@ export default function Navlink({
   ...rest
 }: NavlinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
