f658ede087560d427fb6cddd27dd4ce779c6e4bf
Felipe Moura <felipemimoura@gmail.com>
2022-03-08 08:27:54 -0300

    feat: adicionado contexto do ReactQuery

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { theme } from "../../styles/theme";
 import { SidebarContextProvider } from "../context/SidebarContext";
+import { makeServer } from "../services/mirage";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarContextProvider>
-        <Component {...pageProps} />
-      </SidebarContextProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarContextProvider>
+          <Component {...pageProps} />
+        </SidebarContextProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
