4b429ca9f93b8ae227baecefe5116394d6e8b80d
Felipe Moura <felipemimoura@gmail.com>
2022-03-09 07:40:45 -0300

    refactor: adicona dados na tabela de forma dinamica

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,6 +22,13 @@ import Header from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  createdAt: string | Date;
+};
+
 export default function UserList() {
   const {
     data,
@@ -30,7 +37,20 @@ export default function UserList() {
   } = useQuery("users", async () => {
     const response = await fetch("http://localhost:3000/api/users");
     const data = await response.json();
-    return data;
+
+    const users = data.users.map((user: User) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }),
+      };
+    });
+    return users;
   });
 
   const isWideView = useBreakpointValue({
@@ -79,16 +99,20 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px="6">
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Text fontWeight="bold">Felipe Moura</Text>
-                      <Text fontSize="sm">felipemimoura@gmail.com</Text>
-                    </Td>
-                    {isWideView && <Td>30 de março de 2022</Td>}
-                  </Tr>
+                  {data.map((user: User) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px="6">
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm">{user.email}</Text>
+                        </Td>
+                        {isWideView && <Td>{user.createdAt} </Td>}
+                      </Tr>
+                    );
+                  })}
                 </Tbody>
               </Table>
               <Pagination />
