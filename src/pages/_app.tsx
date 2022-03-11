1858a51a75ad9bc14251ed083574e2084eecb74a
Felipe Moura <felipemimoura@gmail.com>
2022-03-11 07:43:07 -0300

    refactor: remove instancia do queryClient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "../../styles/theme";
 import { SidebarContextProvider } from "../context/SidebarContext";
 import { makeServer } from "../services/mirage";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { queryClient } from "../services/queryClient";
 
 if (process.env.NODE_ENV === "development") {
   makeServer();
 }
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
